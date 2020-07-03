43226b5de856931dda38e99ab217bd921bc11fc2
Stephen Van de Keere <umvan293@gmail.com>
2020-07-02 21:37:05 -0400

    Jokebot can store and repeat jokes from an array

--- Main.py
+++ Main.py
@@ -1,15 +1,28 @@
 userString = ''
-joke = ''
 knockCounter = 0
-jokeBook = []
+jokeBook = [[], []]
 while userString != 'exit':
     userString = input("Whatsup I'm JokeBot! ")
-    if userString == 'JokeBot Tell me a joke':
-        userString = input('I don’t know any jokes yet, can you tell me a Knock knock joke?')
-        joke = userString
-        userString = input('Who is there?')
-        joke = joke + ' ' + userString
+    if userString == 'JokeBot tell me a joke':
+        if len(jokeBook[0]) == 0:
+            userString = input('I don’t know any jokes yet, can you tell me a Knock knock joke?')
+            jokeBook[knockCounter].append(userString + ' ')
+            userString = input('Who’s there?')
+            jokeBook[knockCounter].append(userString + ' ')
+            userString = input(userString + ' who?')
+            jokeBook[knockCounter].append(userString + ' ')
+            print('lmao you so funny')
+            knockCounter = knockCounter + 1
+        else:
+            userString = input(jokeBook[0][0])
+            userString = input(jokeBook[0][1])
+            userString = input(jokeBook[0][2])
+
+    elif userString == 'knock knock':
+        jokeBook[knockCounter].append(userString + ' ')
+        userString = input('Who’s there?')
+        jokeBook[knockCounter].append(userString + ' ')
         userString = input(userString + ' who?')
-        joke = joke + ' ' + userString
+        jokeBook[knockCounter].append(userString + ' ')
         print('lmao you so funny')
-        jokeBook.append(joke)
+        knockCounter = knockCounter + 1
