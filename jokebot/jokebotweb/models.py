fa2f5e380747b987c1bc9e7dcab6ad86d30321d7
Stephen Van de Keere <umvan293@gmail.com>
2020-07-06 21:11:24 -0400

    Updated models and views

--- jokebot/jokebotweb/models.py
+++ jokebot/jokebotweb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
 # Create your models here.
+
+class jokeStorage(models.Model):
+	#knockKnock = models.CharField("Knock Knock")
+	firstPhrase = models.CharField(max_length=256)
+	fullJoke = models.CharField(max_length=256)
+	#jokeCount = models.AutoField(default=0)
+	def __str__(self):
+		return self.fullJoke
+		
+
+
+
--- jokebot/jokebotweb/views.py
+++ jokebot/jokebotweb/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def jokebot(request):
 	return render(request, 'userInterface.html',{})
 	
+def buttonPressed(userInput):
+	return userInput
 	
