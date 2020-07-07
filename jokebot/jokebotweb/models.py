5d54d8b1d89e832a1cb780c277ea2b1dc52a1b95
Stephen Van de Keere <umvan293@gmail.com>
2020-07-06 21:17:50 -0400

    WebPage displays again

--- jokebot/jokebotweb/models.py
+++ jokebot/jokebotweb/models.py
@@ -2,10 +2,9 @@ from django.db import models
 # Create your models here.
 
 class jokeStorage(models.Model):
-	#knockKnock = models.CharField("Knock Knock")
 	firstPhrase = models.CharField(max_length=256)
 	fullJoke = models.CharField(max_length=256)
-	#jokeCount = models.AutoField(default=0)
+	#jokeCount = models.IntField(default=0)
 	def __str__(self):
 		return self.fullJoke
 		
